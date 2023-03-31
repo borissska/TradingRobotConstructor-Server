e14f506e6106909ac40c09fd9b098439635b75df
Boris Okhota <95587232+borissska@users.noreply.github.com>
2023-03-31 18:04:55 +0500

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-db_host = 'localhost'
-db_name = 'tickersData'
-db_user = 'root'
-db_pass = 'root'
-
-bybit_api_key = 'HhuwZBcXiv4LXzFbTu'
-bybit_api_secret = 'V0WWxsEwwEmvbkn4FTP6bvhijcoZmGRuglKL'
-binance_api_key = 'h7LD7BbhlRwxlPaFopaUeIVvyrHFG7zJtQ4XyqKKdIPsbA3EZnqOrZGxNJpmwtyE'
-binance_api_secret = 'b7V0PnflBJHUePc2zZaeTUDzdZ7PntDgn2Bs8KHykjyFh2xyC47kzzSxG7UGNpAU'
